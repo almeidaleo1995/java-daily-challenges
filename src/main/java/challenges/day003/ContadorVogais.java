b417e80e11deae50333b6c3777b404c6b442b2a9
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-08-11 14:10:25 -0300

    finish test

---
+++ src/main/java/challenges/day003/ContadorVogais.java
@@ -0,0 +1,20 @@
+package challenges.day003;
+
+import java.util.List;
+
+public final class ContadorVogais {
+
+  private ContadorVogais() {
+  }
+
+  public static int contarVogais(String texto) {
+    List<Character> vogais = List.of('a', 'e', 'i', 'o', 'u');
+    int contador = 0;
+    for (char c : texto.toLowerCase().toCharArray()) {
+      if (vogais.contains(c)) {
+        contador++;
+      }
+    }
+    return contador;
+  }
+}
---
+++ src/test/java/challenges/day004/CompactadorTest.java
@@ -0,0 +1,48 @@
+package challenges.day004;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CompactadorTest {
+
+  @Test
+  void textoVazio() {
+    assertEquals("", Compactador.compactar(""));
+  }
+
+  @Test
+  void umCaractere() {
+    assertEquals("a1", Compactador.compactar("a"));
+  }
+
+  @Test
+  void semRepeticao() {
+    assertEquals("a1b1c1", Compactador.compactar("abc"));
+  }
+
+  @Test
+  void comRepeticoes() {
+    assertEquals("a3b2c1", Compactador.compactar("aaabbc"));
+  }
+
+  @Test
+  void repeticaoNaoConsecutivaNaoAgrupa() {
+    assertEquals("a2b2a2", Compactador.compactar("aabbaa"));
+  }
+
+  @Test
+  void diferenciaMaiusculaDeMinuscula() {
+    assertEquals("a1A1", Compactador.compactar("aA"));
+  }
+
+  @Test
+  void blocoFinalLongo() {
+    assertEquals("x1y5", Compactador.compactar("xyyyyy"));
+  }
+
+  @Test
+  void espacosTambemContam() {
+    assertEquals("a1 2b1", Compactador.compactar("a  b"));
+  }
+}

405102b34ade63be0d8d2af20d5dda2ae40f0dac
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-08-18 13:45:48 -0300

    done test

---
+++ src/main/java/challenges/day007/ContadorFrequencia.java
@@ -0,0 +1,18 @@
+package challenges.day007;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class ContadorFrequencia {
+
+  private ContadorFrequencia() {}
+
+  public static Map<Integer, Integer> contarFrequencia(List<Integer> numeros) {
+    Map<Integer, Integer> mapa = new HashMap<>();
+    for (Integer numero : numeros) {
+      mapa.put(numero, mapa.getOrDefault(numero, 0) + 1);
+    }
+    return mapa;
+  }
+}

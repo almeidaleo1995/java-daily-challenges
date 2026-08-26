f6d6ce1144c7385263e61d4378e4157f47c1d2d5
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-08-26 09:53:17 -0300

    finish challeges 12

--- src/main/java/challenges/day011/ContadorDigitos.java
+++ src/main/java/challenges/day011/ContadorDigitos.java
@@ -3,6 +3,13 @@ package challenges.day011;
 public final class ContadorDigitos {
 
   public static int contarDigitos(int numero) {
-    throw new UnsupportedOperationException("TODO: implementar");
+    int contador = 0;
+
+    do {
+      numero /= 10;
+      contador++;
+    } while (numero != 0);
+
+    return contador;
   }
 }
--- src/main/java/challenges/day012/ContadorMaiusculas.java
+++ src/main/java/challenges/day012/ContadorMaiusculas.java
@@ -2,7 +2,14 @@ package challenges.day012;
 
 public final class ContadorMaiusculas {
 
+  private ContadorMaiusculas() {}
+
   public static int contarMaiusculas(String texto) {
-    throw new UnsupportedOperationException("TODO: implementar");
+    int contador = 0;
+    for (int i = 0; i < texto.length(); i++) {
+      char letra = texto.charAt(i);
+      if (Character.isUpperCase(letra)) contador++;
+    }
+    return contador;
   }
 }

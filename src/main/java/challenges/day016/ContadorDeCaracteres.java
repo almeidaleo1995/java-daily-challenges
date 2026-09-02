e99e116689fc0eafab213baedaec4b40a94b2660
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-09-02 09:46:11 -0300

    finish desafio 16

--- src/main/java/challenges/day016/ContadorDeCaracteres.java
+++ src/main/java/challenges/day016/ContadorDeCaracteres.java
@@ -1,10 +1,19 @@
 package challenges.day016;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public final class ContadorDeCaracteres {
 
+  private ContadorDeCaracteres() {
+  }
+
   public static Map<Character, Integer> contarCaracteres(String texto) {
-    throw new UnsupportedOperationException("TODO: implementar");
+    Map<Character, Integer> mapa = new HashMap<>();
+    for (char c : texto.toCharArray()) {
+      mapa.put(c, mapa.getOrDefault(c, 0) + 1);
+    }
+
+    return mapa;
   }
 }

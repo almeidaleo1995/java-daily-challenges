ed69a2b6f6205b568382800caf99e73abf30c5a4
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-09-04 10:05:14 -0300

    add news tests

--- src/main/java/challenges/day019/InverteNumero.java
+++ src/main/java/challenges/day019/InverteNumero.java
@@ -5,6 +5,12 @@ public final class InverteNumero {
   private InverteNumero() {}
 
   public static int inverter(int numero) {
-    throw new UnsupportedOperationException("TODO: implementar");
+    int numeroInvertido = 0;
+
+    while (numero != 0) {
+      numeroInvertido = (numeroInvertido * 10) + (numero % 10);
+      numero /= 10;
+    }
+    return numeroInvertido;
   }
 }

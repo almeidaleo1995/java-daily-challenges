14d2a345d379aa82816488c38e029c211455fdd1
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-09-21 09:41:31 -0300

    finish tests

--- src/main/java/challenges/day028/SegundoMaiorValor.java
+++ src/main/java/challenges/day028/SegundoMaiorValor.java
@@ -5,6 +5,23 @@ public final class SegundoMaiorValor {
   private SegundoMaiorValor() {}
 
   public static int segundoMaior(int[] numeros) {
-    throw new UnsupportedOperationException("TODO: implementar");
+    if (numeros.length > 0) {
+      int segundoMaior = Integer.MIN_VALUE;
+      int melhor = Integer.MIN_VALUE;
+
+      for (int i = 0; i < numeros.length; i++) {
+        int atual = numeros[i];
+        if (atual > melhor) {
+          segundoMaior = melhor;
+          melhor = atual;
+        } else if (atual > segundoMaior) {
+          segundoMaior = atual;
+        }
+      }
+
+      return segundoMaior;
+    }
+
+    return 0;
   }
 }

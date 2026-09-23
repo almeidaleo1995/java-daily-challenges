bdf8fffcdb9472b12b73588c63926553721e37e1
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-09-23 09:13:45 -0300

    done test 31

--- src/main/java/challenges/day031/ValoresComuns.java
+++ src/main/java/challenges/day031/ValoresComuns.java
@@ -1,12 +1,21 @@
 package challenges.day031;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class ValoresComuns {
 
   private ValoresComuns() {}
 
   public static List<Integer> comuns(List<Integer> a, List<Integer> b) {
-    throw new UnsupportedOperationException("TODO: implementar");
+    Set<Integer> setAuxilEncontrados = new LinkedHashSet<>();
+    for (Integer numero : a) {
+      if (b.contains(numero)) {
+        setAuxilEncontrados.add(numero);
+      }
+    }
+    return new ArrayList<>(setAuxilEncontrados);
   }
 }

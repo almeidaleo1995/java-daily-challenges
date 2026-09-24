1ac08778d6bb24ad18d0f1210bac461826e1e323
Leonardo Almeida <almeidaleo1995@gmail.com>
2026-09-24 08:46:13 -0300

    done test 34

--- src/main/java/challenges/day034/ContadorDePalavras.java
+++ src/main/java/challenges/day034/ContadorDePalavras.java
@@ -1,5 +1,6 @@
 package challenges.day034;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +9,10 @@ public final class ContadorDePalavras {
   private ContadorDePalavras() {}
 
   public static Map<String, Integer> contarPalavras(List<String> palavras) {
-    throw new UnsupportedOperationException("TODO: implementar");
+    Map<String, Integer> mapa = new HashMap<>();
+    for (String palavra : palavras) {
+      mapa.put(palavra, mapa.getOrDefault(palavra, 0) + 1);
+    }
+    return mapa;
   }
 }
